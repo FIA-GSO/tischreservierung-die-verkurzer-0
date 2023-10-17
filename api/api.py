c3cc8d261fb899fdc06f99cf2ee159935c85eea0
hkxgit <hendrik.koudijs@rahm.de>
2023-10-17 08:40:22 +0200

    added index html

--- api/api.py
+++ api/api.py
@@ -1,12 +1,28 @@
+import os
+
 import flask
 from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
 from flask import jsonify   # übersetzt python-dicts in json
+from flask import Response
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
 
+def root_dir():
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
 @app.route('/', methods=['GET'])
-def home():
-    return "<h1>Tischreservierung hier</h1>"
+def metrics():  # pragma: no cover
+    content = get_file('index.html')
+    return Response(content, mimetype="text/html")
+
 
 app.run()
